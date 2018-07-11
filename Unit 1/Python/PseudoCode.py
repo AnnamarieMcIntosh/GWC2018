88cbd4cb461709460592ff19448ba5309b0f7ca1
AnnamarieMcIntosh <amm@vmcintoshcpa.com>
2018-07-11 15:33:54 -0700

    changes

--- Unit 1/Python/PseudoCode.py
+++
@@ -1,8 +0,0 @@
-# Given a list of ages, calculate the average
-ages = [16,22,20,19,22,11,17]
-#Find the sum (Loop through the elements of the age list)
-sum = 0
-for a in ages:
-    sum += a
-# Find the number of elements in our aList
-count = len(ages)
