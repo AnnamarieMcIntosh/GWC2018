c7db07094d19eaf8844c67d5d68625f898b8c09b
AnnamarieMcIntosh <amm@vmcintoshcpa.com>
2018-07-17 15:55:12 -0700

    hey

--- GWC/Unit 2/Data/data_vis.py
+++ GWC/Unit 2/Data/data_vis.py
@@ -13,8 +13,26 @@ tweetFile = open("TwitterData/tweets.json", "r")
 tweetData = json.load(tweetFile)
 tweetFile.close()
 
+polarity_values = []
+
+for tweet in tweetData:
+    tweet_text = tweet["text"]
+    tb = TextBlob(tweet_text)
+    print ("{}: {}".format(tweet_text, tb.polarity))
+    polarity_values.append(tb.polarity)
+
+bins = [-1, -0.5, 0, 0.5, 1] # 4 bins
+
+plt.hist(polarity_values, bins)
+plt.title("Tweet Polarity")
+plt.ylabel("Number of Tweets")
+plt.xlabel("Polarity")
+plt.show()
+
 # Continue your program below!
 
-# Textblob sample:
-tb = TextBlob("You are a brilliant computer scientist.")
-print(tb.polarity)
+# # Textblob sample:
+# while True:
+#     phrase = input("You are a brilliant computer scientist.")
+#     tb = TextBlob(phrase)
+#     print(tb.polarity)
